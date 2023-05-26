12afb4a5b1f7a12fc9706abd0831bc99202463cb
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-05-26 21:15:56 +0300

    Pешение задачи 24 (64)

---
+++ Task 24/Program.cs
@@ -0,0 +1,13 @@
+﻿// 24. Программа, которая выведет все натуральные числа в промежутке от N до 1 с помощью рекурсии.
+
+void Numbers (int startNum, int endNum)
+{
+    if (startNum==endNum-1)
+    return;
+    Console.Write($"{startNum} ");
+    startNum--;
+    Numbers(startNum,endNum);
+}
+int N=8;     
+int M=1;
+Numbers(N,M);
