2e972c37077fa4e7f56ea7f1a744f65170d39c60
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-05-26 21:29:16 +0300

    Черновик задачи 25 (66)

---
+++ Task 25/Program.cs
@@ -0,0 +1,20 @@
+﻿// 25. Программа, которая находит сумму натуральных элементов в промежутке от M до N
+// с помощью рекурсии.
+
+void Sum (int M, int N)
+{
+    int result=M;
+    if (M==N)
+    return 0;
+    else 
+    {
+        M++;
+        result=M+Sum(M,N);
+        return result;
+    }
+    Console.Write($"{result} ");
+    Sum(M,N);
+}
+int N=8;     
+int M=1;
+Sum(M,N);
